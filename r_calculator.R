7357c7d18d2d5b937123d56480cc520509047436
10353830 <10353830@mydbs.ie>
2017-05-10 16:44:21 +0100

    Added power function

--- r_calculator.R
+++ r_calculator.R
@@ -14,36 +14,41 @@ multiply <- function(x, y) {
 }
 #Divide Function
 divide <- function(x, y) {
-  if (num2 == 0){
+  if (num2 == 0)
     return("Cannot divide by zero")
-  }
   else
     return(x / y)
+}
+#Power Function
+power <- function(x, y) {
+  return(x^y)
+}
+#Square Function
+sqrt <-function(x) {
+  return (x*x)
 }
 #Exponential Function
 exp <- function(x, y) {
   return(x ** y)
 }
-#Square Function
-square <-function(x) {
-  return (x*x)
-}
+
 #Show Menu
 print("Make Selection for calculation")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
-print("5.Exponential")
-print("6.Square")
+print("5.Power")
+print("6.Exponential")
+print("7.Square Root")
 # Get user input
 Selection = as.integer(readline(prompt="Enter your selection: "))
 num1 = as.numeric(readline(prompt="Enter number for calculation: "))
-if (Selection <6 ) {
+if (Selection <7 ) {
   num2 = as.numeric(readline(prompt="Enter second number: "))
 }
 operator <- switch(Selection,"+","-","*","/","to Exponent of","The Square of")
-result <- switch(Selection, add(num1, num2), subtract(num1, num2), multiply(num1, num2), divide(num1, num2), exp(num1, num2),square(num1))
-if (Selection <6){
+result <- switch(Selection, add(num1, num2), subtract(num1, num2), multiply(num1, num2), divide(num1, num2),power(num1, num2), exp(num1, num2),sqrt(num1))
+if (Selection <7){
   print(paste(num1, operator, num2, "=", result))
 } else print(paste( operator, num1, "=", result))
