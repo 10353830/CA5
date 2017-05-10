2e7188037795cfcfff06700f5bc69331ce0bbcc3
10353830 <10353830@mydbs.ie>
2017-05-10 18:54:20 +0100

    Last commit went backwards??

--- r_calculator.R
+++ r_calculator.R
@@ -1,5 +1,6 @@
 #Derek Baker 10353830
 
+#Do calculations
 #Add Function
 add <- function(x, y) {
   return(x + y)
@@ -24,12 +25,17 @@ power <- function(x, y) {
   return(x^y)
 }
 #Square Function
-sqrt <-function(x) {
-  return (x*x)
+sqrts <-function(x) {
+  return (sqrt(x))
 }
+
 #Exponential Function
 exp <- function(x, y) {
   return(x ** y)
+}
+#Sine Function
+sine <- function(x) {
+  return(sin(x))
 }
 
 #Show Menu
@@ -41,14 +47,19 @@ print("4.Divide")
 print("5.Power")
 print("6.Exponential")
 print("7.Square Root")
+print("8.Sin")
+
+
 # Get user input
 Selection = as.integer(readline(prompt="Enter your selection: "))
 num1 = as.numeric(readline(prompt="Enter number for calculation: "))
-if (Selection <7 ) {
+if (Selection <=6 ) {
   num2 = as.numeric(readline(prompt="Enter second number: "))
 }
-operator <- switch(Selection,"+","-","*","/","to Exponent of","The Square of")
-result <- switch(Selection, add(num1, num2), subtract(num1, num2), multiply(num1, num2), divide(num1, num2),power(num1, num2), exp(num1, num2),sqrt(num1))
-if (Selection <7){
+#Get output
+operator <- switch(Selection,"+","-","*","/","To the Power of", "to Exponent of","The Square Root of","Sin of")
+result <- switch(Selection, add(num1, num2), subtract(num1, num2), multiply(num1, num2), divide(num1, num2)
+                 ,power(num1, num2), exp(num1, num2), sqrts(num1), sine(num1))
+if (Selection <=6){
   print(paste(num1, operator, num2, "=", result))
 } else print(paste( operator, num1, "=", result))
